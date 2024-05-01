004369441c792e0b62e7629436cb3244f526f3b7
Iryna Howarth <irynahowarth@gmail.com>
2024-05-01 13:32:32 +0100

    [feature]: add MainLayout and pull Metadata from Prismic

--- src/prismic.ts
+++ src/prismic.ts
@@ -12,3 +12,18 @@ export const client = prismic.createClient(repositoryName, {
     },
   ],
 })
+
+export async function getPage(uuid: string) {
+  return client.getByUID('page', uuid, { pageSize: 1, page: 1 });
+}
+
+export async function generateMetadata() {
+  const {data} = await client.getSingle("settings");
+  return {
+    title: data.site_title || "Flowrise",
+    description: data.meta_description || "Flowrise is relaxing app for you",
+    openGraph: {
+      image: [data.og_image.url || ""]
+    }
+  }
+}
