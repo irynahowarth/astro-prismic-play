7c693135086986fd7f85c99faf0d24f02aa695ed
Iryna Howarth <irynahowarth@gmail.com>
2024-05-01 13:58:04 +0100

    [feature]: add Header and Footer components

--- src/prismic.ts
+++ src/prismic.ts
@@ -17,13 +17,15 @@ export async function getPage(uuid: string) {
   return client.getByUID('page', uuid, { pageSize: 1, page: 1 });
 }
 
-export async function generateMetadata() {
-  const {data} = await client.getSingle("settings");
+export async function generateSetting() {
+  const { data } = await client.getSingle("settings");
+
   return {
     title: data.site_title || "Flowrise",
     description: data.meta_description || "Flowrise is relaxing app for you",
     openGraph: {
       image: [data.og_image.url || ""]
-    }
+    },
+    navigation: data.navigation
   }
 }
